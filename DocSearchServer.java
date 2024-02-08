f269b272d6167fc273d2932db9f47996c4707056
Ferretelic <kyleshouki@gmail.com>
2024-02-08 13:45:50 -0800

    add two queries

--- DocSearchServer.java
+++ DocSearchServer.java
@@ -37,37 +37,59 @@ class Handler implements URLHandler {
     this.base = Paths.get(directory);
   }
 
+  List<File> searchByTitle(List<File> paths, String title) throws IOException {
+    List<File> foundPaths = new ArrayList<>();
+    for (File f : paths) {
+      if (f.toString().contains(title)) {
+        foundPaths.add(f);
+      }
+    }
+    return foundPaths;
+  }
+
+  List<File> searchByQuery(List<File> paths, String query) throws IOException {
+    List<File> foundPaths = new ArrayList<>();
+    for (File f : paths) {
+      if (FileHelpers.readFile(f).contains(query)) {
+        foundPaths.add(f);
+      }
+    }
+    return foundPaths;
+  }
+
   public String handleRequest(URI url) throws IOException {
     List<File> paths = FileHelpers.getFiles(this.base);
     if (url.getPath().equals("/")) {
       return String.format("There are %d total files to search.", paths.size());
     } else if (url.getPath().equals("/search")) {
-      String[] parameters = url.getQuery().split("=");
-      if (parameters[0].equals("q")) {
-        String result = "";
-        List<String> foundPaths = new ArrayList<>();
-        for (File f : paths) {
-          if (FileHelpers.readFile(f).contains(parameters[1])) {
-            foundPaths.add(f.toString());
-          }
+      List<File> foundPaths = null;
+      if (url.getQuery().contains("&")) {
+        String[] parameters = url.getQuery().split("&");
+        String[] queryParameters = parameters[0].split("=");
+        String[] titleParameters = parameters[1].split("=");
+        if (queryParameters[0].equals("q") && titleParameters[0].equals("title")) {
+          foundPaths = this.searchByQuery(paths, queryParameters[1]);
+          foundPaths = this.searchByTitle(foundPaths, titleParameters[1]);
+        } else {
+          return "Couldn't find query parameter q and title";
         }
-        Collections.sort(foundPaths);
-        result = String.join("\n", foundPaths);
-        return String.format("Found %d paths:\n%s", foundPaths.size(), result);
-      } else if (parameters[0].equals("title")) {
-        String result = "";
-        List<String> foundPaths = new ArrayList<>();
-        for (File f : paths) {
-          if (f.toString().contains(parameters[1])) {
-            foundPaths.add(f.toString());
-          }
-        }
-        Collections.sort(foundPaths);
-        result = String.join("\n", foundPaths);
-        return String.format("Found %d paths:\n%s", foundPaths.size(), result);
       } else {
-        return "Couldn't find query parameter q";
+        String[] parameters = url.getQuery().split("=");
+        if (parameters[0].equals("q")) {
+          foundPaths = this.searchByQuery(paths, parameters[1]);
+        } else if (parameters[0].equals("title")) {
+          foundPaths = this.searchByTitle(paths, parameters[1]);
+        } else {
+          return "Couldn't find query parameter q";
+        }
       }
+      List<String> results = new ArrayList<String>();
+      for (File file : foundPaths) {
+        results.add(file.toString());
+      }
+      Collections.sort(results);
+      String result = String.join("\n", results);
+      return String.format("Found %d paths:\n%s", foundPaths.size(), result);
     } else {
       return "Don't know how to handle that path!";
     }
