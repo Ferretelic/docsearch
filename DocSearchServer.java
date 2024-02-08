b6fb2830d81d05d4847678d9e591872cdfd72b3d
Ferretelic <kyleshouki@gmail.com>
2024-02-08 13:32:16 -0800

    add title search

--- DocSearchServer.java
+++ DocSearchServer.java
@@ -11,68 +11,79 @@ import java.util.List;
 import java.util.Collections;
 
 class FileHelpers {
-    static List<File> getFiles(Path start) throws IOException {
-        File f = start.toFile();
-        List<File> result = new ArrayList<>();
-        if (f.isDirectory()) {
-            File[] paths = f.listFiles();
-            for (File subFile : paths) {
-                result.addAll(getFiles(subFile.toPath()));
-            }
-        } else {
-            result.add(start.toFile());
-        }
-        return result;
+  static List<File> getFiles(Path start) throws IOException {
+    File f = start.toFile();
+    List<File> result = new ArrayList<>();
+    if (f.isDirectory()) {
+      File[] paths = f.listFiles();
+      for (File subFile : paths) {
+        result.addAll(getFiles(subFile.toPath()));
+      }
+    } else {
+      result.add(start.toFile());
     }
+    return result;
+  }
 
-    static String readFile(File f) throws IOException {
-        return new String(Files.readAllBytes(f.toPath()));
-    }
+  static String readFile(File f) throws IOException {
+    return new String(Files.readAllBytes(f.toPath()));
+  }
 }
 
 class Handler implements URLHandler {
-    Path base;
+  Path base;
 
-    Handler(String directory) throws IOException {
-        this.base = Paths.get(directory);
-    }
+  Handler(String directory) throws IOException {
+    this.base = Paths.get(directory);
+  }
 
-    public String handleRequest(URI url) throws IOException {
-        List<File> paths = FileHelpers.getFiles(this.base);
-        if (url.getPath().equals("/")) {
-            return String.format("There are %d total files to search.", paths.size());
-        } else if (url.getPath().equals("/search")) {
-            String[] parameters = url.getQuery().split("=");
-            if (parameters[0].equals("q")) {
-                String result = "";
-                List<String> foundPaths = new ArrayList<>();
-                for (File f : paths) {
-                    if (FileHelpers.readFile(f).contains(parameters[1])) {
-                        foundPaths.add(f.toString());
-                    }
-                }
-                Collections.sort(foundPaths);
-                result = String.join("\n", foundPaths);
-                return String.format("Found %d paths:\n%s", foundPaths.size(), result);
-            } else {
-                return "Couldn't find query parameter q";
-            }
-        } else {
-            return "Don't know how to handle that path!";
+  public String handleRequest(URI url) throws IOException {
+    List<File> paths = FileHelpers.getFiles(this.base);
+    if (url.getPath().equals("/")) {
+      return String.format("There are %d total files to search.", paths.size());
+    } else if (url.getPath().equals("/search")) {
+      String[] parameters = url.getQuery().split("=");
+      if (parameters[0].equals("q")) {
+        String result = "";
+        List<String> foundPaths = new ArrayList<>();
+        for (File f : paths) {
+          if (FileHelpers.readFile(f).contains(parameters[1])) {
+            foundPaths.add(f.toString());
+          }
         }
+        Collections.sort(foundPaths);
+        result = String.join("\n", foundPaths);
+        return String.format("Found %d paths:\n%s", foundPaths.size(), result);
+      } else if (parameters[0].equals("title")) {
+        String result = "";
+        List<String> foundPaths = new ArrayList<>();
+        for (File f : paths) {
+          if (f.toString().contains(parameters[1])) {
+            foundPaths.add(f.toString());
+          }
+        }
+        Collections.sort(foundPaths);
+        result = String.join("\n", foundPaths);
+        return String.format("Found %d paths:\n%s", foundPaths.size(), result);
+      } else {
+        return "Couldn't find query parameter q";
+      }
+    } else {
+      return "Don't know how to handle that path!";
     }
+  }
 }
 
 class DocSearchServer {
-    public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
-            System.out.println(
-                    "Missing port number or directory! The first argument should be the port number (Try any number between 1024 to 49151) and the second argument should be the path of the directory");
-            return;
-        }
-
-        int port = Integer.parseInt(args[0]);
-
-        Server.start(port, new Handler(args[1]));
+  public static void main(String[] args) throws IOException {
+    if (args.length < 2) {
+      System.out.println(
+          "Missing port number or directory! The first argument should be the port number (Try any number between 1024 to 49151) and the second argument should be the path of the directory");
+      return;
     }
+
+    int port = Integer.parseInt(args[0]);
+
+    Server.start(port, new Handler(args[1]));
+  }
 }
